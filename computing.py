8903938ebeb2984e358df8ebf4c6479816907915
bhavik9988 <57974417+bhavik9988@users.noreply.github.com>
2020-10-03 12:51:50 +0530

    Add files via upload

---
+++ computing.py
@@ -0,0 +1,12 @@
+def computepay(h,r):
+    return 42.37
+x=input("enter no. of hours:")
+hrs=int(x)
+y=input("enter no. of rate:")
+rate=float(y)
+if hrs<40:
+    pay=hrs*rate
+elif hrs>=40:
+    pay=(40*rate)+(hrs-40)*rate*1.5
+
+print("Pay",pay)
