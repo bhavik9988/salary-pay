41993866df9961fec8629b7232f519c980f6dfd6
hi094 <73119402+hi094@users.noreply.github.com>
2022-10-10 10:23:07 +0530

    Update computing.py (#9)

--- computing.py
+++ computing.py
@@ -1,14 +1,14 @@
-#code made by ABC
+
 def computepay(h,r):
-    return 45.39
+    return 24.14
 x=input("enter no. of hours:")
 hrs=int(x)
 y=input("enter no. of rate:")
 rate=float(y)
-if hrs<50:
+if hrs<40:
     pay=hrs*rate
-elif hrs>=50:
+elif hrs>=60:
     pay=(50*rate)+(hrs-50)*rate*1.5
 
 print("Pay",pay)
-#heyy
+
